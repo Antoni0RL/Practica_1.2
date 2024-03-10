520d6cdc510e6d2c6c42714bd8bb4247d29036e1
Hael1324 <161646503+Hael1324@users.noreply.github.com>
2024-03-10 16:33:57 -0600

    Update MezclaDeJugos1.js

--- MezclaDeJugos1.js
+++ MezclaDeJugos1.js
@@ -1,3 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('¿Qué bebida va a pedir?: ', (jugo1) => {
 let jugo1 = window.prompt("Que bebida va pedir: ")
 let jugo = jugo1.toLowerCase()
 let lista_jugos = ["pure strawberry joy", "energizer", "green garden", "tropical island", "all or nothing", "oferta especial"]
@@ -16,3 +24,49 @@ function timeToMixJuice(jugo){
 }
 
 timeToMixJuice(jugo)
+
+function limesToCut(neededSlices, limes) {
+    let totalSlices = 0;
+    let i = 0;
+
+    while (totalSlices < neededSlices && i < limes.length) {
+      switch (limes[i]) {
+        case "small":
+          totalSlices += 6;
+          break;
+        case "medium":
+          totalSlices += 8;
+          break;
+        case "large":
+          totalSlices += 10;
+          break;
+      }
+
+      i++;
+    }
+
+    return i;
+  }
+
+  function PedidosRestantes(minutesLeft, orders) {
+    let remainingOrders = [];
+    let i = 0;
+
+    do {
+      const orderTime = timeToMixJuice(orders[i]);
+      minutesLeft -= orderTime;
+
+      if (minutesLeft < 0) {
+        remainingOrders.push(orders[i]);
+      }
+
+      i++;
+    } while (i < orders.length && minutesLeft > 0);
+
+    return remainingOrders;
+  }
+
+  timeToMixJuice(jugo);
+
+  rl.close();
+});
